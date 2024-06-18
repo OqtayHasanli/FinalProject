aa5186e0d47f5b1ffa0e56559414cdec79616e2d
OqtayHasanli <okyhasanli@gmail.com>
2024-06-18 03:09:43 -0700

    profile

--- frontend/src/layout/navbar/Navbar.jsx
+++ frontend/src/layout/navbar/Navbar.jsx
@@ -1,37 +1,44 @@
-import React from 'react'
-import "./navbar.scss"
-import logo from "../../assets/images/logo.png"
-import { Link } from 'react-router-dom'
-import { FaInstagram } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
-import { FaYoutube } from "react-icons/fa";
-
-
+import React, { useState } from 'react';
+import "./navbar.scss";
+import logo from "../../assets/images/logo.png";
+import { Link } from 'react-router-dom';
+import {FaCircleUser } from "react-icons/fa6";
 
 const Navbar = () => {
+  const [isProfileVisible, setProfileVisible] = useState(false);
+
+  const toggleProfileVisibility = () => {
+    setProfileVisible(!isProfileVisible);
+  };
+
   return (
     <div className='mainnav'>
       <div className='containNavbar'>
-        <img className='navbarimage' src={logo} alt="" />
+        <img className='navbarimage' src={logo} alt="Logo" />
         <ul className='navbarul'>
-
           <Link className='linknav' to={"home"}> <li className='navlist'>Home</li></Link>
           <li className='navlist'>About</li>
           <li className='navlist'>Gallery</li>
           <li className='navlist'>Contacts</li>
           <Link className='linknav' to={"shop"}> <li className='navlist'>Shop</li></Link>
         </ul>
-        <div className='socialnavbar'>
-          <FaInstagram className='navsocial' />
-          <FaTwitter className='navsocial' />
-          <FaYoutube className='navsocial' />
-
+        <div className='socialnavbar' onClick={toggleProfileVisibility}>
+          <FaCircleUser className='profilebtn' />
+          <p>Profile</p>
         </div>
-
+        {isProfileVisible && (
+          <div className='profilepage'>
+            <h3>gulsan@gmail.com</h3>
+            <div className='basketfav'>
+              <button className='mybasket'>My Basket</button>
+              <button className='myfavorite'>My Favorite</button>
+            </div>
+            <button className='logutbtn'>Log out</button>
+          </div>
+        )}
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
